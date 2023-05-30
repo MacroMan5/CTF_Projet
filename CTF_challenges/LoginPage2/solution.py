af6d6c805305725c8c14d3aaae452aa1b1a71311
MacroMan5 <119378668+MacroMan5@users.noreply.github.com>
2023-05-30 12:17:38 -0400

    Update solution.py
    ajout des valeurs a chaque iteration dans la console

--- CTF_challenges/LoginPage2/solution.py
+++ CTF_challenges/LoginPage2/solution.py
@@ -9,11 +9,15 @@ def generate_password():
     z = ["MTA=", "MjA=", "MzA=", "NDA="]
     
     xyz = []
-    for value in z:
+    n = 1
+    for value in z:  
         zz = int(base64.b64decode(value).decode('utf-8'))
+        sum_value = zz + x + y
+        print(f"Valeur {n} ajouté au mot de passe complet, x, and y: {sum_value}")
         xyz.append(str(zz + x + y))
-    
+        n += 1
     password = ''.join(xyz)
+    print(f"Mot de passe complet: {password}")
     return password
 
 password = generate_password()
