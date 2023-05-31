30086277a7cd2efb5ccf4b137f4b977987134522
MacroMan5 <119378668+MacroMan5@users.noreply.github.com>
2023-05-31 10:53:56 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,32 @@
 
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Challenges</title>
+</head>
+<body>
+  <h1>Challenges</h1>
+  <div id="challenges"></div>
+
+  <script>
+    // Fetch the list of challenges from the server
+    fetch('/challenges')
+      .then(response => response.json())
+      .then(challenges => {
+        // Get the challenges container
+        const challengesDiv = document.getElementById('challenges');
+
+        // For each challenge, create a button and append it to the container
+        challenges.forEach(challenge => {
+          const button = document.createElement('button');
+          button.textContent = `Start Challenge ${challenge.name}`;
+          button.onclick = function() {
+            // When button is clicked, make a request to start the challenge
+            fetch(`/challenges/${challenge.id}/start`);
+          };
+          challengesDiv.appendChild(button);
+        });
+      });
+  </script>
+</body>
+</html>
